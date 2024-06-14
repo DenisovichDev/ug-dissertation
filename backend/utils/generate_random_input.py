b4e2361979335b6d600dcb374fde13feb92678b1
DenisovichDev <bhaswarishi2908@gmail.com>
2024-06-14 15:17:55 +0530

    Small changes to random graph

--- backend/utils/generate_random_input.py
+++ backend/utils/generate_random_input.py
@@ -1,4 +1,4 @@
-import random
+import random, sys
 
 def generate_weighted_graph(num_vertices, num_edges, filename="data/input/graph.txt"):
     edges = set()
@@ -26,6 +26,9 @@ def generate_weighted_graph(num_vertices, num_edges, filename="data/input/graph.
     print("]")
 
 # Example usage
-num_vertices = 100
-num_edges = 200 # Adjust this number to make the graph larger
-generate_weighted_graph(num_vertices, num_edges)
+if __name__ == "__main__":
+    # Adjust manually or command line argument
+    # Argument #1 is vertices, #2 is edges
+    num_vertices = 100 if len(sys.argv) <= 1 else int(sys.argv[1])
+    num_edges = 200 if len(sys.argv) <= 1 else int(sys.argv[2])
+    generate_weighted_graph(num_vertices, num_edges)
