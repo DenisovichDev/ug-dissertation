bc41f54a699dc07359b89df9d9feb1bee6b1119a
DenisovichDev <bhaswarishi2908@gmail.com>
2024-06-14 11:17:01 +0530

    wrapping up

--- backend/utils/generate_random_input.py
+++ backend/utils/generate_random_input.py
@@ -12,20 +12,20 @@ def generate_weighted_graph(num_vertices, num_edges, filename="data/input/graph.
 
     with open(filename, 'w') as f:
         f.write(f"u\n")
-        print("edges: ")
+        print("\"edges\" : ", end="")
         print("[", end="")
         for u, v, weight in edges:
             f.write(f"{u} {v} {weight}\n")
             print(f"[{u}, {v}, {weight}],", end=" ")
         print("]")
 
-    print("nodes: ")
+    print("\"nodes\" : ", end="")
     print("[", end="")
     for i in range(num_vertices):
         print(i, end=", ")
     print("]")
 
 # Example usage
-num_vertices = 10
-num_edges = 20 # Adjust this number to make the graph larger
+num_vertices = 100
+num_edges = 200 # Adjust this number to make the graph larger
 generate_weighted_graph(num_vertices, num_edges)
